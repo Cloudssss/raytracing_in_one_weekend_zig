ae1ff1a7fa7dc0d124067c0fb9119f77d1b5144a
Cloudlpx <Cloudlpx@Gmail.com>
2024-08-19 22:40:05 +0800

    feat: 补全向量运算的函数

--- src/vec3.zig
+++ src/vec3.zig
@@ -36,7 +36,7 @@ pub const Vec3 = struct {
     }
 
     pub fn add(a: Self, b: Self) Self {
-        return vec3(a.x() + b.x(), a.y() + b.y(), a.y() + b.y());
+        return vec3(a.e[0] + b.e[0], a.e[1] + b.e[1], a.e[2] + b.e[2]);
     }
 
     pub fn addAssign(a: *Self, b: Self) *Self {
@@ -47,7 +47,7 @@ pub const Vec3 = struct {
     }
 
     pub fn sub(a: Self, b: Self) Self {
-        return vec3(a.x() - b.x(), a.y() - b.y(), a.y() - b.y());
+        return vec3(a.e[0] - b.e[0], a.e[1] - b.e[1], a.e[2] - b.e[2]);
     }
 
     pub fn subAssign(a: *Self, b: Self) *Self {
@@ -57,11 +57,23 @@ pub const Vec3 = struct {
         return a;
     }
 
+    pub fn multiply(a: Self, b: Self) Self {
+        return vec3(a.e[0] * b.e[0], a.e[1] * b.e[1], a.e[2] * b.e[2]);
+    }
+
+    pub fn multiplyNum(a: Self, b: f64) Self {
+        return vec3(a.e[0] * b, a.e[1] * b, a.e[2] * b);
+    }
+
     pub fn multiplyAssign(a: *Self, t: f64) *Self {
         a.e[0] *= t;
         a.e[1] *= t;
         a.e[2] *= t;
         return a;
+    }
+
+    pub fn dividedByNum(a: Self, b: f64) Self {
+        return vec3(a.e[0] * (1 / b), a.e[1] * (1 / b), a.e[2] * (1 / b));
     }
 
     pub fn dividedByAssign(a: *Self, t: f64) *Self {
@@ -74,6 +86,18 @@ pub const Vec3 = struct {
 
     pub fn lengthSquared(self: Self) f64 {
         return self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2];
+    }
+
+    pub fn dot(a: Self, b: Self) f64 {
+        return a.e[0] * b.e[0] + a.e[1] * b.e[1] + a.e[2] * b.e[2];
+    }
+
+    pub fn cross(u: Self, v: Self) Self {
+        return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1], u.e[2] * v.e[0] - u.e[0] * v.e[2], u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+    }
+
+    pub fn unitVector(self: Self) Self {
+        return self.dividedByNum(self.length());
     }
 };
 
