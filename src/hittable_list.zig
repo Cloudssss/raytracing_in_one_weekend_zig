977b80a537770b099d631ad1f8a6e6ed273f7ae0
Cloudlpx <Cloudlpx@Gmail.com>
2024-08-19 22:40:05 +0800

    feat: 可命中对象列表结构体

---
+++ src/hittable_list.zig
@@ -0,0 +1,45 @@
+const std = @import("std");
+const Ray = @import("ray.zig").Ray;
+const Hittable = @import("hittable.zig").Hittable;
+const HitRecord = @import("hittable.zig").HitRecord;
+const ArrayList = @import("std").ArrayList;
+
+const HittableList = struct {
+    objects: ArrayList(Hittable),
+
+    const Self = @This();
+
+    // TO-DO 之后看看这里的gpa会不会失效
+    pub fn init() Self {
+        var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+        const allocator = gpa.allocator();
+        return .{
+            .objects = ArrayList(Hittable).init(allocator),
+        };
+    }
+
+    pub fn deinit(self: *Self) void {
+        self.objects.deinit();
+    }
+
+    pub fn add(self: *Self, object: Hittable) !void {
+        try self.objects.append(object);
+    }
+
+    pub fn hit(self: Self, r: Ray, ray_tmin: f64, ray_tmax: f64, rec: *HitRecord) bool {
+        var temp_hit_record: HitRecord = undefined;
+        const temp_rec = &temp_hit_record;
+        var hit_anything = false;
+        var closest_so_far = ray_tmax;
+
+        for (self.objects.items) |object| {
+            if (object.hit(r, ray_tmin, closest_so_far, temp_rec)) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                rec.* = temp_rec.*;
+            }
+        }
+
+        return hit_anything;
+    }
+};
